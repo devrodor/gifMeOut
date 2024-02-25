5a6d2ea5799ae9fcc1cfaad58713a84cd9a7956c
devrodor <rodorgm@gmail.com>
2024-02-25 18:52:46 +0100

    fetching Giphy

--- src/GifMeOutApp.jsx
+++ src/GifMeOutApp.jsx
@@ -1,86 +1,79 @@
 import { useState } from "react"
-import Grid from '@mui/material/Grid';
+import { Container, Grid, AppBar, Toolbar, Box, Typography } from '@mui/material';
+import ReactLogo from './assets/images/react.svg';
 import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
- 
+
+//Styles
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import './assets/css/style.css'
+
+//themes
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const basicTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#454545',
+        textBackground: '#fcfbe0'
+      },
+      // Puedes definir otros colores aquí
+    },
+  });
+
+// app
 export const GifMeOutApp = () => {
 
-    /* 
 
-        REMEMBER: La desestructuración es contínua y muchas veces puede ser confuso saber qué se está extrayendo
-
-        Vamos a establecer una variable de estado 'categories' en el componente. 
-        Inicialmente, categories es undefined porque useState() se ha llamado sin un valor inicial. 
-        
-        setCategories es una función que puedes usar para cambiar el valor de categories, 
-        y cualquier cambio en categories hará que el componente se vuelva a renderizar con el nuevo estado.
-
-        DESGLOSE
-
-        useState() es una "variable de estado". A diferencia de las variables normales de JavaScript, 
-                cuando usamos una variable de estado, el componente se vuelve a renderizar automáticamente.
-        
-        useState() devuelve un par de valores, que asignamos a categories y setCategories.
-
-        categories es la variable de estado actual. Al inicio, su valor es el que se pasa a useState(), 
-        en este caso, como no se ha pasado ningún valor, será undefined.
-
-        setCategories es una función que permite actualizar categories. 
-        Cada vez que se llama a setCategories, el componente se vuelve a renderizar, 
-        y categories tendrá el nuevo valor que le hayas pasado a setCategories.
-
-    */
-
-    // const addCategory = ( ) => {
-    //     setCategories([...categories, 'The Smile']); // y para esto se usa el spread, metemos las categorías existentes y añadimos la nueva
-    // }
-
-    const [ categories, setCategories ] = useState([ 'Heavy Metal', 'Cats' ]);
+    const [ categories, setCategories ] = useState([ 'Heavy Metal']);
 
     const onAddCategory = ( newCategory ) => { //
 
-        //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
-
-        /* 
-        
-        El método includes() determina si un array incluye un determinado elemento, devuelve true o false según corresponda.
-        
-        */
-
-        /* Podríamos validar estrictamente el valor con mayusculas o minusculas u otros caracteres, pero para React, 
-            son valores diferentes y por tanto admitidos en este caso. No complication
-        */
-
         if( categories.includes( newCategory ) )return; //full stop, categoría ya existe.
-
         setCategories([ newCategory, ...categories ]);
     }
 
     return (
         <>
-        <Grid container  
-            direction="column"
-            >
-            <Grid item m={12}> 
+        <ThemeProvider theme={basicTheme}>
 
-                {/* Titulo */}
-                <h1>GifMeOutApp</h1>
+        {/* Menu superior */}
+        <AppBar position="static" elevation={0}>
+            <Toolbar>
+            <Box display="flex" alignItems="center" flexGrow={1}>
+                <img src={ReactLogo} alt="React Logo" style={{ marginRight: 20, height: 40 }} />
+                <Typography variant="h6" noWrap component="div">
+                GifMeOut
+                </Typography>
+            </Box>
+            </Toolbar>
+        </AppBar>
 
-                {/* Input */}
-                {/* El nombre del metodo setCategories es random, simplemente para ordenar */}
-                {/* Cuando el componente está emitiendo algo, se usa el prefijo on por convención */}
-                <AddCategory 
-                    /* La prop onNewCategory es una función. 
-                    / Esta función es una envoltura alrededor de onAddCategory, que está definida en el componente padre. 
-                     Cada vez que onNewCategory es llamada en el hijo, llamará a onAddCategory en el padre con el mismo argumento que recibe. */
-                    onNewCategory={ ( cat ) => onAddCategory( cat )  } // la info llega desde el componente hijo y se emite y envia a fn.
-                /> 
-                
+        {/* Contenedor principal */}
+        <Container maxWidth="lg" sx={{ mt: 2, mb: 2, p:1 }}>
+
+        <Grid container spacing={2}>
+        <Grid item xs={12}>
+            {/* Titulo */}
+            <h1>GifMeOutApp</h1>
+        </Grid>
+        <Grid item m={4} sx={{ mt: 0, mb: 0, p:0 }}>  
+
+            {/* Input + Functionality */} 
+            <AddCategory 
+                onNewCategory={ ( el ) => onAddCategory( el )  } 
+            /> 
+            </Grid>
+                <Grid item m={8} sx={{ mt: 0, mb: 0, p:0 }}>
                 { 
-                    categories.map( ( category ) => (
+                    categories.map( ( cat ) => (
                         <GifGrid 
-                            key={ category } 
-                            category={ category } 
+                            key={ cat } 
+                            category={ cat } 
                         />
                     )) 
                 }
@@ -88,6 +81,8 @@ export const GifMeOutApp = () => {
 
             </Grid> 
         </Grid>
+        </Container>
+        </ThemeProvider>
         </>
         
     )
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -4,33 +4,9 @@ import { Button } from "@mui/material";
 
 export const AddCategory = ({ onNewCategory }) => { //remember, aqui recibimos las props y las desestructuramos, una de ellas es la fn setCats
 
-        /* 
-
-        Vamos a establecer una variable de estado 'inputValue' en el componente. 
-        Inicialmente, inputValue es undefined porque useState() se ha llamado sin un valor inicial. 
-        
-        setinputValue es una función que puedes usar para cambiar el valor de inputValue, 
-        y cualquier cambio en inputValue hará que el componente se vuelva a renderizar con el nuevo estado.
-
-        DESGLOSE
-
-        useState() es una "variable de estado". A diferencia de las variables normales de JavaScript, 
-                cuando usamos una variable de estado, el componente se vuelve a renderizar automáticamente.
-        
-        useState() devuelve un par de valores, que asignamos a inputValue y setinputValue.
-
-        inputValue es la variable de estado actual. Al inicio, su valor es el que se pasa a useState(), 
-        en este caso, como no se ha pasado ningún valor, será undefined.
-
-        setinputValue es una función que permite actualizar inputValue. 
-        Cada vez que se llama a setinputValue, el componente se vuelve a renderizar, 
-        y inputValue tendrá el nuevo valor que le hayas pasado a setinputValue.
-
-    */
-
     const [inputValue, setInputValue] = useState('');
     //const onInputChange = (event) => {
-        // ojo, aquí estamos desestructurando event.target . Aquí siempre recibimos event como argumento
+    // desestructurando event.target 
     const onInputChange = ({ target }) => { 
         console.log(target.value);
         setInputValue(target.value);
@@ -39,29 +15,30 @@ export const AddCategory = ({ onNewCategory }) => { //remember, aqui recibimos l
 
     const onSubmit = (event) => {
         event.preventDefault();  
-        if( inputValue.trim().length <= 1 ) return;//si no hay nada en el input, full stop 
+        if( inputValue.trim().length <= 1 ) return;
 
         onNewCategory( inputValue.trim() ); //emitimos el valor al padre a través del parámetro del hijo el hijoparametralpater :)
-        setInputValue(''); //para limpiar el input una vez enviado
+        setInputValue(''); //clean input after send
     }
 
 
     return(
         <Box 
-        sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+        sx={
+            { '& .MuiTextField-root': { m: 0 }, 
+            display: 'flex' }
+        }
         noValidate
         autoComplete="off"
         >
         <form onSubmit={ (event) => onSubmit(event) }>
             <div>
-                <TextField
-                    //llamamos a estos atributos 'properties'
+                <TextField 
                     id="outlined-required"
+                    sx={{ flex: 1 }}
                     label="Enter element list"
-                    value={ inputValue }
-                    // ya sabemos que una función a la que pasamos como argumento el mismo argumento a retornar se resume 
-                    //onChange={ (event) =>  onInputChange(event) } // cambiamos esto a ...
-                    onChange={ onInputChange }
+                    value={ inputValue } 
+                    onChange={ onInputChange } //onChange={ (event) =>  onInputChange(event) } 
                 />
                 
                 {/* Listado de Gifs */}
